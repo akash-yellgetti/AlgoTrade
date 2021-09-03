8d10fa89c7eec052f25dd52215fb5d7414000ea4
Akash Y <akash.yellgetti@bts.com>
2021-09-03 17:39:30 +0530

    Layout component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { RightSidebarComponent } from './shared/layout/right-sidebar/right-sidebar.component';
+import { LeftSidebarComponent } from './shared/layout/left-sidebar/left-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RightSidebarComponent,
+    LeftSidebarComponent
   ],
   imports: [
     BrowserModule
