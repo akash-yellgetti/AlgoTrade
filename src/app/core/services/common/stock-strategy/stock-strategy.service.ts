36e4ad3c7e836822f622f3def0c53f15b38df744
Akash Y <akash.yellgetti@bts.com>
2021-09-03 16:24:33 +0530

    stock-market module added

--- src/app/core/services/common/stock-strategy/stock-strategy.service.ts
+++ src/app/core/services/common/stock-strategy/stock-strategy.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockStrategyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchStock(text): Observable<any> {
+    const url = 'https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxCompanySearch.jsp?search='+text;
+    const headers = new HttpHeaders({
+      'X-Requested-With': 'XMLHttpRequest',
+      'Referer': 'https://www.nseindia.com/ChartApp/install/charts/mainpage.jsp',
+      Host: 'www.nseindia.com'
+    });
+
+    const options = { headers: headers };
+    return this.http.get(url, options)
+      .pipe(map((data) => data),
+        catchError((err) => this.handleError(err, 'not found')));
+  }
+
+  private handleError(error: any, message: any): any {
+    // this.showLoader = false;
+    // this.toastr.error(message);
+
+    switch (error.code) {
+      case 400:
+      case 409:
+        for (const err of error.error) {
+          // this.toastr.error(err.message);
+        }
+        break;
+      default:
+        // this.toastr.error(message);
+    }
+    return throwError(error.error || 'Server error');
+  }
 }
---
+++ src/app/modules/stock-market/stock-market.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StockMarketRoutingModule } from './stock-market-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StockMarketRoutingModule
+  ]
+})
+export class StockMarketModule { }
