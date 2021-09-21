ab3806ad08170eb5a95b0258f876c5b28a88001d
Akash Y <akash.yellgetti@bts.com>
2021-09-21 23:09:41 +0530

    changes for hosting

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+//setting middleware
+// app.use(express.static(__dirname + 'docs')); //Serves resources from public folder
+// app.get('/', (req, res) => res.send('Node Hello World!'))
+
+
+app.use(express.static('docs'));
+
+//Serves all the request which includes /images in the url from Images folder
+app.use('/', express.static(__dirname + '/docs'));
+
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    // tslint:disable:no-console
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', (err) => {
+    console.error(err, 'Uncaught Exception thrown');
+    // process.exit(1);
+  });
+
+
+app.listen(port, () => console.log('Example app listening on port `${port}`!'))
